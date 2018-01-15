7ef2902bf94b8da8546ea670f46ddfa3ae295ec5
Petar <34079554+ptrsploit@users.noreply.github.com>
2018-01-15 15:32:48 +0100

    Media Player

---
+++ mplayer.java
@@ -0,0 +1,37 @@
+package amedia;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+
+public class Amedia extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("Moj medija plejer");
+        
+        scene.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent doubleClicked) {
+               if(doubleClicked.getClickCount() == 2){
+                   stage.setFullScreen(true);
+               }
+            }
+        });
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
